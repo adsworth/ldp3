55351a52d8d352bd95b060027f75954976a922e9
Adi J. Sieker <adi@sieker.io>
2014-02-24 14:57:45 +0100

    check for 0 before division.

--- ldp/skater/models.py
+++ ldp/skater/models.py
@@ -52,4 +52,7 @@ class SkaterProfile(UserenaBaseProfile):
     
     @cached_property
     def avg_speed(self):
-        return "%0.2f" %(self.total_distance / self.total_duration.seconds * ( 60 *60 ))
+        if self.total_duration.seconds > 0:
+            return "%0.2f" %(self.total_distance / self.total_duration.seconds * ( 60 *60 ))
+        else:
+            return ""
