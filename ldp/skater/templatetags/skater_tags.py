54e2e1d7e83d6150762f4373bc20429f33fc1f64
Adi J. Sieker <adi@sieker.io>
2014-03-12 22:04:48 +0100

    display random skater on front page.

---
+++ ldp/skater/templatetags/skater_tags.py
@@ -0,0 +1,19 @@
+from datetime import datetime, date, time
+
+from django import template
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from skater.models import privacy_exclude_for_user
+
+
+register = template.Library()
+
+@register.inclusion_tag('skater/random_skater.html')
+def random_skater(user):
+    skater = User.objects.exclude(id=settings.ANONYMOUS_USER_ID).exclude(profile__privacy__in=privacy_exclude_for_user(user)).order_by('?')[:1]
+    if len(skater) <> 0:
+        skater = skater[0]
+
+    return {'skater':  skater }
+
